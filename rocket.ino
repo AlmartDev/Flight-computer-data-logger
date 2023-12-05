605700271792e6d667821114808f8e0f833ceb4f
AlmartDev <alonsomartinezcarratala@gmail.com>
2023-12-04 20:07:54 -0800

    Commit barometric sensor, servos and termometer system

--- rocket.ino
+++ rocket.ino
@@ -2,10 +2,23 @@
 
 #include <RH_RF95.h>
 
+#include <Servo.h>
+
+#include <Adafruit_BMP085.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+int startAltitude, altitude;
+int startPressure;
+int temperature;
+
 RH_RF95 driver;
+Servo servo1, servo2;
+
+// Inits -----------------------------
 
 void initComms() {
-    Serial.println("Initializing LoRa comms");
+    Serial.println("Initializing LoRa comms ----------------");
     if (!driver.init()) {
         Serial.println("ERROR");
         // ShutDown();
@@ -13,8 +26,39 @@ void initComms() {
     Serial.println("done.");
     driver.setFrequency(915.0);
 
-    sendData("LoRa Comms inited");
+    sendData("LoRa Comms inited ----------------");
 }
+
+void initServos() {
+    sendData("Initializing Servos");
+    // Serial.println("Initializing Servos");
+
+    servo1.attach(A0);
+    servo2.attach(A1);
+
+    sendData("done.");
+    // Serial.println("done.");
+}
+
+void initBarometric() {
+    sendData("Initializing Barometric sensors");
+    // Serial.println("Initializing Barometric sensors");
+
+    if (!barometer.begin()) {
+        sendData("ERROR");
+        // Serial.println("ERROR");
+        while (1) {}
+    }
+
+    sendData("done.");
+    // Serial.println("done.");
+
+    // Read presure and calculate altitude at start
+    startPressure = barometer.readPressure();
+    startAltitude = barometer.readAltitude(startPressure);
+}
+
+// Inits -----------------------------
 
 void sendData(const char data = "No data.") {
     driver.send((uint8_t *)data, strlen(data));
@@ -33,19 +77,42 @@ void recieveData() {
     } 
 }
 
+// Updates -----------------------------
+
+void updateBarometric() {
+    temperature = barometer.readTemperature();
+
+    // altitude calculation
+    //                     Actual altitude (sea level)      Start Altitude (sea level)
+    float altitude = barometer.readAltitude(startPressure) - startAltitude;     // this will give the change in the altitude
+
+    // Serial.println(altitude);
+
+    sendData(temperature);
+    sendData(altitude);
+}
+
+// Updates -----------------------------
+
+
 void setup() {
-  Serial.begin(9600);
-  initComms();
+  Serial.begin(9600); // only used for debugging
+
+  initComms();          // Communication
+  initServos();         // Servos
+  initBarometric();     // Barometric sensor/temperature
 }
 
 void loop() {
-  // Examples
+    // Updates
+    updateBarometric();
 
-  // sendData(temperature);
+    // Examples
+    // sendData(temperature);
 
-  //    if (recieveData() == "parachute") {
-  //        deployParachute();
-  //    }
+    //    if (recieveData() == "parachute") {
+    //        deployParachute();
+    //    }
 
-  delay(500);
+    delay(500);
 }
