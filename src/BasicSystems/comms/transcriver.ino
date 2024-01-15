c03d14cbebf17057a198bc04f826f9717ca7a791
AlmartDev <alonsomartinezcarratala@gmail.com>
2024-01-15 10:16:23 -0800

    Update transcriver.ino

--- src/BasicSystems/comms/transcriver.ino
+++ src/BasicSystems/comms/transcriver.ino
@@ -1,71 +1,45 @@
 #include <LoRa.h>
  
-#include <Adafruit_BMP085.h>
-#include <Wire.h>
- 
- 
- 
-float startAltitude, altitude;
-float startPressure;
- 
-Adafruit_BMP085 barometer;
-  
-void initBarometric() {
-    Serial.println("Initializing Barometric sensors");
- 
-    if (!barometer.begin()) {
-        Serial.println("ERROR");
-        while (1) {}
-    }
-    else
-        Serial.println("done.");
- 
-    startPressure = barometer.readPressure();
-    startAltitude = barometer.readAltitude(startPressure);
-}
-
-void initCommms()
-{
-    Serial.print("Initializing LoRa comms  -  ");
-    //SendData("Initializing LoRa comms  -  ");
-
-    if (!LoRa.begin(915E6))  // initialize ratio at 915 MHz
-    { 
-        Serial.println("ERROR");
-        //SendData("ERROR");
-        while (true)
-            ;
-    }
-    Serial.println("done.");
-    //SendData("done.");
-}
+#define LORA_SS 10
+#define LORA_RST 9
+#define LORA_DIO0 2
  
 void setup() {
   Serial.begin(9600);
+  while (!Serial);
  
-  initCommms();
-  initBarometric();
-}
-
-template <typename remoteData>
-void SendData(remoteData data)  // sends data to ground station, all data will be send in a new line
-{
-    LoRa.beginPacket();
-    LoRa.print(String(data));
-    LoRa.endPacket();
+  // Initialize LoRa module
+  if (!LoRa.begin(433E6)) { // Change the frequency to match your LoRa module's frequency
+    Serial.println("LoRa initialization failed. Check your wiring!");
+    while (1);
+  }
+ 
+  Serial.println("LoRa Transceiver Initialized");
 }
  
 void loop() {
+  String message = "Hello, LoRa!";
  
-  altitude = barometer.readAltitude(startPressure);  
+  // Send message
+  LoRa.beginPacket();
+  LoRa.print(message);
+  LoRa.endPacket();
  
-  Serial.println(altitude - startAltitude);
-  //Serial.println (" - t:");
-  //Serial.print(temperature);
+  Serial.println("Message sent: " + message);
  
-  float realAltitude = altitude - startAltitude;
-  
-  SendData("Testttt!!!!");
-  
-  delay(1000); // Delay before sending the next message
+  // Wait for some time before sending the next message
+  delay(5000);
+ 
+  // Check for incoming messages
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    Serial.println("Received a message:");
+ 
+    // Read the message
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+ 
+    Serial.println();
+  }
 }
