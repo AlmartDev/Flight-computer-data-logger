4a878ab1b370c4ce810c028df373f58fa11a8c49
AlmartDev <alonsomartinezcarratala@gmail.com>
2024-01-15 10:16:07 -0800

    Update reciver.ino

--- src/BasicSystems/comms/reciver.ino
+++ src/BasicSystems/comms/reciver.ino
@@ -1,27 +1,34 @@
-#include <SPI.h>
 #include <LoRa.h>
+ 
+#define LORA_SS 10
+#define LORA_RST 9
+#define LORA_DIO0 2
  
 void setup() {
   Serial.begin(9600);
   while (!Serial);
  
-  if (!LoRa.begin(433E6)) {
-    Serial.println("Starting LoRa failed!");
+  // Initialize LoRa module
+  if (!LoRa.begin(433E6)) { // Change the frequency to match your LoRa module's frequency
+    Serial.println("LoRa initialization failed. Check your wiring!");
     while (1);
   }
-  else
-    Serial.println("done");
+ 
+  Serial.println("LoRa Receiver Initialized");
 }
  
 void loop() {
+  // Check for incoming messages
   int packetSize = LoRa.parsePacket();
+ 
   if (packetSize) {
-    Serial.println("Received packet!");
+    Serial.println("Received a message:");
+ 
+    // Read the message
     while (LoRa.available()) {
-      String receivedData = LoRa.readString();
-      Serial.println("Received: " + receivedData);
+      Serial.print((char)LoRa.read());
     }
  
-    Serial.println("-------------");
+    Serial.println();
   }
 }
